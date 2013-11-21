b35205864646346092904304a5ceea602da5df56
Kobra <c0bra@xiatek.org>
2013-11-21 00:01:22 +0000

    Begining

--- lua/ar_minedexer.lua
+++ lua/ar_minedexer.lua
@@ -1,1 +1,13 @@
+minedexer = {}
 
+local httpstatus = require("httpstatus")
+local configor = require("configor")
+
+local template = include("template.lua")
+
+minedexer.index = function(req, res)
+	template.generate(res, "Minedexer", {"Hello, world!"})
+end
+
+
+reqs.AddPattern("*", "/minedexer/", minedexer.index)
--- lua/template.lua
+++ lua/template.lua
@@ -1,1 +1,19 @@
 
+local template = {}
+
+template.generate = function(res, title, content)
+	tags.html 
+	{
+		tags.head
+		{
+			tags.title { tostring(title) },
+			tags.link { type="text/css", rel="stylesheet", href="/minedexer/style.css" }
+		},
+		tags.body
+		{
+			unpack(content)
+		}
+	}.to_response(res)
+end
+
+return template
